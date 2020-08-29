46bd71090bd77a1ab958bbd93b9d6b93c97bf53d
Pan Theis <pan.theis@rohde-schwarz.com>
2020-08-29 10:23:07 +0200

    Add dark mode toggle scripts.

---
+++ dark mode.bat
@@ -0,0 +1,1 @@
+powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force; New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
---
+++ light mode.bat
@@ -0,0 +1,1 @@
+powershell New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force; New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
